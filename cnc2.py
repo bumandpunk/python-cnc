09e092e320022ae0fbeef14332787d5e7f94e1d0
zhangfujie <zhangfujie@workcell.com>
2024-04-25 10:54:43 +0800

    first commit

---
+++ cnc2.py
@@ -0,0 +1,90 @@
+'''
+Date: 2024-04-20 09:53:25
+LastEditors: Zfj
+LastEditTime: 2024-04-20 16:09:01
+FilePath: /cool-admin-vue/Users/zfj/Desktop/test.py
+Description: 
+'''
+# https://u.cewaycloud.com/scan-code?short-chain=/aE2ayUYeaa
+import os
+import shutil
+import tkinter as tk
+from tkinter import ttk
+import psutil
+import get_name2
+import threading
+import time
+def find_usb_drives():
+    """查找并返回所有连接的USB驱动器的挂载路径。"""
+    # drives = []
+    # for part in psutil.disk_partitions():
+    #     if 'removable' in part.opts:
+    #         drives.append(part.mountpoint)
+    # print(drives[0])
+    # return drives[0]
+    return r"\\192.168.3.11\udisk"
+
+def copy_folder(src, dest):
+    """复制文件或文件夹到目标路径，并更新进度条"""
+    if os.path.isdir(src):
+        files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(src) for f in filenames]
+        total_files = len(files)
+        copied_files = 0
+
+        if not os.path.exists(dest):
+            os.makedirs(dest, exist_ok=True)
+        
+        for file in files:
+            dest_file = file.replace(src, dest)
+            dest_dir = os.path.dirname(dest_file)
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir, exist_ok=True)
+            shutil.copy2(file, dest_file)
+            copied_files += 1
+    elif os.path.isfile(src):
+        if not os.path.exists(os.path.dirname(dest)):
+            os.makedirs(os.path.dirname(dest), exist_ok=True)
+        shutil.copy2(src, dest)
+
+
+def download_file():
+  
+    file_names = get_name2.main()  # 需要实现这个函数
+
+    usb_drives = find_usb_drives()  # 查找USB驱动器路径列表
+    if not usb_drives:
+        print("No USB drives detected.")
+        return
+
+    # 选择第一个USB驱动器进行操作
+    destination_drive = usb_drives
+    # destination_drive = usb_drives[0]
+    print("Using USB drive:", destination_drive,file_names)
+    
+    # 共享文件夹路径
+    # share_path = r"\\10.10.30.2\1"
+    share_path = r"\\192.168.9.3\DD2023\部门文件\CNC加工部"
+    if file_names:
+    # 为每个文件进行复制操作
+     
+    #  for file_name in file_names:
+        source_folder = os.path.join(share_path, file_names)
+        destination = os.path.join(destination_drive, file_names)
+        print("Source:", source_folder)
+        print("Destination:", destination)
+        
+        if os.path.exists(source_folder):
+            copy_folder(source_folder, destination)
+            print("Copy operation completed successfully for:", file_names)
+        else:
+            print("Specified source does not exist for:", file_names)
+    
+    
+
+def repeat_func():
+    while True:
+        download_file()
+        time.sleep(5)
+        
+t = threading.Thread(target=repeat_func)
+t.start()
---
+++ get_name2.py
@@ -0,0 +1,142 @@
+'''
+Date: 2024-04-20 13:46:01
+LastEditors: Zfj
+LastEditTime: 2024-04-20 15:41:27
+FilePath: /cool-admin-vue/get_name.py
+Description: 
+'''
+import json
+import base64
+import requests
+from datetime import datetime, timedelta
+from urllib.parse import urlparse, parse_qs
+# Basic authentication with base64 encoding
+def btoa(text):
+    return base64.b64encode(text.encode('utf-8')).decode('utf-8')
+
+basic_auth = f"Basic {btoa('social:social')}"
+
+# Fetch token for API authentication
+def fetch_token(username, password):
+    url = 'https://tp.cewaycloud.com/auth/oauth/token?randomStr=blockPuzzle&code=&grant_type=password'
+    headers = {
+        'accept': 'application/json',
+        'authorization': basic_auth,
+        'content-type': 'application/x-www-form-urlencoded',
+        'platform-id': '1689154431733325826',
+        'tenant-id': '1660451255092543490'
+    }
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code == 200:
+    #    print('获取token成功')
+       return response.json()['access_token'], response.json()['user_id']
+        
+    else:
+        print('Error fetching token:', response.text)
+        return None
+
+
+# Fetch file names from the CNC table using code from shortChain data
+def fetch_form_data(token, value):
+    # url = 'https://tp.cewaycloud.com/fd/formInstance/page'
+    # headers = {
+    #     'accept': 'application/json',
+    #     'authorization': f'Bearer {token}',
+    #     'content-type': 'application/json;charset=UTF-8',
+    #     'platform-id': '1689154431733325826',
+    #     'tenant-id': '1660451255092543490'
+    # }
+    # data_payload = {
+    #     "templateId": "1780225248372080640",
+    #     "current": 1,
+    #     "size": 10,
+    #     "queryFieldList": [{"fieldName": "a171350602412975401", "fieldValue": value, "operType": "fuzzy"}]
+    # }
+    # response = requests.post(url, headers=headers, json=data_payload)
+    # if response.status_code == 200:
+    #     records = response.json().get('data', {}).get('records', [])
+    #     if records:
+    #         original_file_names = [item['originalFileName'] for item in json.loads(records[0]['a171350603547099936'])]
+    #         print('文件列表', original_file_names)
+            # return original_file_names
+            return value
+    #     else:
+    #         print('暂无需要下载文件')
+    # else:
+    #     print('Error fetching form data:', response.text)
+
+# 轮询列表
+def fetch_data(token):
+    url = 'https://tp.cewaycloud.com/fd/formInstance/page'
+    headers = {
+        'accept': 'application/json',
+        'authorization': f'Bearer {token}',
+        'content-type': 'application/json;charset=UTF-8',
+        'platform-id': '1689154431733325826',
+        'tenant-id': '1660451255092543490'
+    }
+    data_payload = {
+        "templateId": "1781639013446074368",
+        "current": 1,
+        "size": 10,
+        "queryFieldList":[{"fieldName":"a171384321153363681","fieldValue":"未完成","operType":"fuzzy"}],
+    }
+    response = requests.post(url, headers=headers, json=data_payload)
+    if response.status_code == 200:
+        records = response.json().get('data', {}).get('records', [])
+        tuhaoList = []
+        if records:
+            for item in records:
+                tuhaoList.append({'tuhao':item['a17138431911882915'],'id':item['id']})
+            
+                print('Query successful:', tuhaoList)
+                return tuhaoList
+        else:
+            print('No records found.')
+    else:
+        print('Error fetching form data:', response.text)
+
+def update_data(token,id):
+    url = 'https://tp.cewaycloud.com/fd/formInstance'
+    headers = {
+        'accept': 'application/json',
+        'authorization': f'Bearer {token}',
+        'content-type': 'application/json;charset=UTF-8',
+        'platform-id': '1689154431733325826',
+        'tenant-id': '1660451255092543490'
+    }
+    data_payload = {
+        "id":id,
+        "templateId":"1781639013446074368",
+        "a171384321153363681":"已完成" 
+    }
+    response = requests.put(url, headers=headers, json=data_payload)
+    if response.status_code == 200:
+          print(id,'修改成功')
+       
+    else:
+        print('Error fetching form data:', response.text)
+def main():
+    username = 'im0204'
+    password = 'JFat0Zdc'
+    token_data = fetch_token(username, password)
+    
+    if token_data:
+        token, user_id = token_data
+         
+        # 图号列表
+        result = fetch_data(token)
+       
+        if result :
+            for item in result:
+                # 更新下载状态
+             print('开始更新下载状态')
+             update_data(token,item['id'])
+            #  fetch_form_data(token, item)
+             return(fetch_form_data(token, item['tuhao']))
+if __name__ == '__main__':
+    main()
